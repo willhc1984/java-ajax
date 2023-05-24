06f7be4d8df6fe6259450f4111f66e8b15943f77
will henrique <willhc1984@gmail.com>
2023-05-24 13:43:22 -0300

    Limpando formulario - link invalido

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -9,14 +9,28 @@ $("#linkPromocao").on('change', function(){
 			method: "POST",
 			url: "/meta/info?url=" + url,
 			cache: false,
+			beforeSend: function(){
+				$("#alert").removeClass("alert alert-danger").text('');
+				$("#titulo").val("");
+				$("#site").text("");
+				$("#linkImagem").attr("src", "/images/promo-dark.png");
+			},
 			success: function(data){
 				console.log(data);
 				$("#titulo").val(data.title);
 				$("#site").text(data.site.replace("@", ""));
 				$("#linkImagem").attr("src", data.image);
-			}
-		});
+			},
+			statusCode: {
+				404: function(){
+					$("#alert").addClass("alert alert-danger").text("Nenhuma informação encontrada");
+					}
+				},
+				error: function(){
+					$("#alert").addClass("alert alert-danger").text("Ops... algo deu errado! Tente mais tarde.");
+				}
+			});
 		
-	}
+		}
 	
 });
