3cf1860a2725212b9fa3b69ea6d2c537e15eca5f
will henrique <willhc1984@gmail.com>
2023-05-29 11:21:12 -0300

    Retorno de paginação pelo controller

--- src/main/java/com/javaajax/controller/PromocaoController.java
+++ src/main/java/com/javaajax/controller/PromocaoController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -62,7 +63,8 @@ public class PromocaoController {
 	@GetMapping("/list")
 	public String listarOfertas(ModelMap model) {
 		Sort sort = Sort.by(Sort.Direction.DESC, "dataCadastro");
-		model.addAttribute("promocoes", promocaoRepository.findAll(sort));
+		PageRequest pageRequest = PageRequest.of(0, 8, sort);
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
 		return "promo-list";
 	}
 
