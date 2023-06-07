cc4aa3a4c3b96ae5cb6fa29a6367a99affb9f6b5
will henrique <willhc1984@gmail.com>
2023-06-07 16:16:58 -0300

    Abrir modal ao selecionar linha em data tables

--- src/main/resources/static/js/promo-datatables.js
+++ src/main/resources/static/js/promo-datatables.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	
-	$("#table-server").DataTable({
+	moment.locale('pt-br');
+	
+	var table = $("#table-server").DataTable({
 		
 		processing: true,
 		serverSide: true,
@@ -17,11 +19,80 @@ $(document).ready(function(){
 			{data: 'linkPromocao'},
 			{data: 'descricao'},
 			{data: 'linkImagem'},
-			{data: 'preco'},
+			{data: 'preco', render: $.fn.dataTable.render.number('.', ',', 2, 'R$')},
 			{data: 'likes'},
-			{data: 'dataCadastro'},
+			{data: 'dataCadastro', render: 
+				function(dataCadastro){
+					return moment(dataCadastro).format('LLL');
+				}
+			},
 			{data: 'categoria.titulo'}
+		],
+		dom: 'Bfrtip',
+		buttons: [
+			{
+				text: 'Editar',
+				attr: {
+					id: 'btn-editar',
+					type: 'button'
+				},
+				enabled: false
+			},
+				
+			{
+				text: 'Excluir',
+				attr: {
+					id: 'btn-excluir',
+					type: 'button'
+				},
+				enabled: false
+			}
 		]
 	});
+	
+	//ação para marcar/desmarcar botoes ao clicar nas ordenações
+	$("#table-server thead").on('click', 'tr', function(){
+			table.buttons().disable();		
+	});
+	
+	//ação para marcar/desmarcar linhas clicadas
+	$("#table-server tbody").on('click', 'tr', function(){
+		
+		if($(this).hasClass('selected')){
+			$(this).removeClass('selected');
+			table.buttons().disable();
+		}else{
+			$('tr.selected').removeClass('selected');
+			$(this).addClass('selected');	
+			table.buttons().enable();		
+		}
+	});
+	
+	//ação do botão de editar
+	$("#btn-editar").on('click', function(){
+				
+		if(isSelectedRow()){
+			$("#modal-form").modal('show');
+			var id = getPromoId();
+		}
+		
+	});
+	
+	//ação do botão de excluir
+	$("#btn-excluir").on('click', function(){
+		if(isSelectedRow()){
+			$("#modal-delete").modal('show');
+			var id = getPromoId();
+		}
+	});
+	
+	function getPromoId(){
+		return table.row(table.$('tr.selected')).data().id;
+	}
+	
+	function isSelectedRow(){
+		var trow =  table.row(table.$('tr.selected'));
+		return trow.data() !== undefined;
+	}
 	
 });
